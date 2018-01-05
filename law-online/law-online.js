048351c105ce880a629938775f01e38a39333a0e
chensheng <chenshsh@syscloud.cn>
2018-01-05 15:57:29 +0800

    update

--- law-online/law-online.js
+++ law-online/law-online.js
@@ -3,6 +3,44 @@
  */
 lawApp = angular.module('lawOnline', ['ionic']);
 
+// 验证码指令
+lawApp.directive('checkcode', function ($interval) {
+    return {
+        restrict: 'EA',
+        scope: true,
+        replace: true,
+        template: '<button class="button button-balanced button-block button-small" ng-disabled="disable">{{content}}</button>',
+        link: function (scope, elements, attrs) {
+            var time = 60;
+            scope.content = '验证码';
+            elements.bind('click', function () {
+                scope.getCode().then(function (res) {
+                    if (res > 0) {
+                        scope.disable = true
+                        scope.content = time
+                        var timer = $interval(timercallback, 1000)
+                    }
+                    function timercallback() {
+                        if (time > 0) {
+                            time--
+                            scope.disable = true
+                            scope.content = time
+                        }
+                        else {
+                            scope.disable = false
+                            scope.content = '验证码'
+                            $interval.cancel(timer);
+                            return time = 60
+                        }
+                    }
+                })
+
+
+            })
+        }
+    }
+});
+
 lawApp.controller('MyCtrl', function ($scope, $ionicSideMenuDelegate, $ionicModal) {
     $scope.toggleRight = function () {
         $ionicSideMenuDelegate.toggleRight();
